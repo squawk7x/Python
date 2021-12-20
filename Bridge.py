25f1838799f8ba1858fba7e6771de97735730878
squawk7x <andreas@ottburg.de>
2021-12-20 18:25:04 +0100

    works with 'Card' objects

--- Bridge.py
+++ Bridge.py
@@ -3,7 +3,6 @@ import random
 suits = ['\u2666', '\u2665', '\u2660', '\u2663']
 ranks = ["6", "7", "8", "9", "10", "J", "Q", "K", "A"]
 
-
 class Card:
 	''' Represents a single playing card '''
 	
@@ -11,18 +10,24 @@ class Card:
 		if suit in suits and rank in ranks:
 			self.suit = suit
 			self.rank = rank
-			self.value = self.setcardvalue(self.rank)
+			self.value = self.setvalue(self.rank)
 	
 	def __str__(self):
-		return '{}{} {}'.format(self.suit, self.rank, self.value)
+		return '{} {}'.format(self.suit, self.rank, self.value)
 	
-	def __eq__(self, other):  # can be
+	def __eq__(self, other):
 		if self.rank == other.rank or self.suit == other.suit or self.rank == 'J':
 			return True
 		else:
 			return False
+		
+	def getsuite(self):
+		return self.suit
+		
+	def getrank(self):
+		return self.rank
 	
-	def setcardvalue(self, rank):
+	def setvalue(self, rank):
 		value = 0
 		if rank in {'10', 'Q', 'K'}: value = 10
 		if rank == 'A': value = 15
@@ -32,10 +37,13 @@ class Card:
 	def getvalue(self):
 		return self.value
 
+#card = Card()
+
+
 
 class Deck:
 	''' Represents a full card deck
-	with an closed drawpile and an open stack '''
+	with a closed drawpile and an open stack '''
 	
 	def __init__(self):
 		self.drawpile = []
@@ -43,7 +51,7 @@ class Deck:
 		
 		for suit in suits:
 			for rank in ranks:
-				self.drawpile.append((suit, rank))
+				self.drawpile.append(Card(suit, rank))
 		
 		self.shuffle_drawpile()
 		
@@ -52,10 +60,12 @@ class Deck:
 		random.shuffle(self.drawpile)
 	
 	def show_drawpile(self):
-		print(self.drawpile)
+		cards = [str(card) for card in self.drawpile]
+		print(cards)
 	
 	def show_stack(self):
-		print(self.stack)
+		cards = [str(card) for card in self.stack]
+		print(cards)
 	
 	def draw_card(self):
 		if len(self.drawpile) == 0:
@@ -64,8 +74,8 @@ class Deck:
 		# random.shuffle(self.drawpile)
 		return self.drawpile.pop()
 	
-	def play_card(self, suit, rank):
-		self.stack.append((suit, rank))
+	def put_card(self, card):
+		self.stack.append(card)
 
 
 deck = Deck()
@@ -77,41 +87,52 @@ class Player:
 	def __init__(self, name):
 		self.hand = []
 		self.possible = []
+		self.name = name
+		
 		for _ in range(5):
 			self.get_card()
-		if name == 'Player 1':
-			deck.play_card(self.hand[4][0], self.hand[4][1])  # Eröffnungskarte
-			self.hand.pop(4)
+			
+		''' Eröffnungskarte '''
+		if self.name == 'Player 1':
+			deck.put_card(self.hand.pop())
+			
+	def show_hand(self):
+		cards = [str(card) for card in self.hand]
+		print(cards)
+		
+	def show_possible(self):
+		cards = [str(card) for card in self.possible]
+		print(cards)
 	
 	def get_card(self):
 		self.hand.append(deck.draw_card())
 		
 	def possible_cards(self):
 		for card in self.hand:
-			if card[0] == deck.stack[-1][0] or card[1] == deck.stack[-1][1] or card[1] == 'J':
+			if card.__eq__(deck.stack[-1]):
 				self.possible.append(card)
 		return self.possible
 		
-	def put_card(self):
+	def play_card(self):
 		pass
 
 
-print('Drawpile:')
+print('Drawpile:', len(deck.drawpile))
 deck.show_drawpile()
-print('Stack: ')
+print('Stack: ', len(deck.stack))
 deck.show_stack()
 
 p1 = Player('Player 1')
-p1.put_card()
-print('Player 1: ', p1.hand)
+print('Player 1: ', p1.show_hand())
 p2 = Player('Player 2')
-print('Player 2: ', p2.hand)
+print('Player 2: ', p2.show_hand())
 
-print('Drawpile:')
+print('Drawpile:', len(deck.drawpile))
 deck.show_drawpile()
-print('Stack: ')
+print('Stack: ', len(deck.stack))
 deck.show_stack()
 
 print('Possible Cards Player 1: ')
-print(p1.possible_cards())
+p1.possible_cards()
+print(p1.show_possible())
 
